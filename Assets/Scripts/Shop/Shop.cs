b066d5e33132af0f44ef18b3466749035e11c74d
Minhyeon <guncraft11@naver.com>
2026-05-08 17:46:35 +0900

    Feat: 메인 신 추가

---
+++ Assets/Scripts/Shop/Shop.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(ShopManager))]
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(SpriteRenderer))]
+
+public class Shop : MonoBehaviour
+{
+    private ShopManager shopManager;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // 상점 아이콘 표시
+        shopManager = GetComponent<ShopManager>(); // 상점 기능
+    }
+
+    private void Update()
+    {
+        // 상점 클릭
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            HandleShopClick();
+    }
+
+    private void HandleShopClick()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+        {
+            if (hit.collider.TryGetComponent<Shop>(out Shop shopIcon))
+            {
+                shopManager.OpenShop();
+            }
+        }
+    }
+}
